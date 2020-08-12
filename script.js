c3cd7b7205e89b2c65d8870e3f464b854f2fe227
Glitch (personality-quiz-starter-code) <none>
2020-08-12 17:30:14 +0000

    🎖🌎 Checkpoint ./index.html:13238183/5061 ./script.js:13238183/1824 ./style.css:13238183/107

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 var questionCount = 0;
 var skywardScore = 0;
 var fountainScore = 0;
-var streetScore = 0;
+var americanScore = 0;
 var beautifulScore = 0;
 
 
@@ -13,23 +13,56 @@ var beautifulScore = 0;
 
 
 //#TODO: Use the DOM to create variables for the first quiz question.
+//questions 1 
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
+
+//questions 2
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 var q2a3 = document.getElementById("q2a3");
 var q2a4 = document.getElementById("q2a4");
+
+//questions 3
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
 
+//questions 4
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
+
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", skywardScore);
+//questions 1
+q1a1.addEventListener("click", skyward);
+q1a2.addEventListener("click", fountain);
+q1a3.addEventListener("click", american);
+q1a4.addEventListener("click", beautiful);
+
+//questions 2
+q2a1.addEventListener("click", beautiful);
+q2a2.addEventListener("click", skyward);
+q2a3.addEventListener("click", american);
+q2a4.addEventListener("click", fountain);
+
+//questions 3
+q3a1.addEventListener("click", beautiful);
+q3a2.addEventListener("click", fountain);
+q3a3.addEventListener("click", american);
+q3a4.addEventListener("click", skyward);
+
+//questions 4
+q4a1.addEventListener("click", fountain);
+q4a2.addEventListener("click", american);
+q4a3.addEventListener("click", skyward);
+q4a4.addEventListener("click", beautiful);
 
 
 
@@ -39,8 +72,22 @@ q1a1.addEventListener("click", skywardScore);
 
 
 //#TODO: Define quiz functions here
-function result1(){
-  
-  
-  
+function skyward(){
+  skywardScore+=1;
+  questionCount +=1;  
+}
+
+function fountain(){
+  fountainScore+=1;
+  questionCount +=1; 
+}
+
+function american(){
+  americanScore+=1;
+  questionCount +=1; 
+}
+
+function beautiful(){
+  beautifulScore+=1;
+  questionCount +=1; 
 }
