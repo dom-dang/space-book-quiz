aa6096d48828a5362de8aeebf92344314d8f21c7
Glitch (personality-quiz-starter-code) <none>
2020-08-14 00:17:39 +0000

    📷👨‍❤️‍💋‍👨 Checkpoint ./script.js:13238183/3267

--- script.js
+++ script.js
@@ -15,42 +15,56 @@ var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
+var q1a5 = document.getElementById("q1a5");
+var q1a6 = document.getElementById("q1a6");
 
 //questions 2
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 var q2a3 = document.getElementById("q2a3");
 var q2a4 = document.getElementById("q2a4");
+var q2a5 = document.getElementById("q2a5");
+var q2a6 = document.getElementById("q2a6");
 
 //questions 3
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
+var q3a5 = document.getElementById("q3a5");
+var q3a6 = document.getElementById("q3a6");
 
 //questions 4
 var q4a1 = document.getElementById("q4a1");
 var q4a2 = document.getElementById("q4a2");
 var q4a3 = document.getElementById("q4a3");
 var q4a4 = document.getElementById("q4a4");
+var q4a5 = document.getElementById("q4a5");
+var q4a6 = document.getElementById("q4a6");
 
 //questions 5
 var q5a1 = document.getElementById("q5a1");
 var q5a2 = document.getElementById("q5a2");
 var q5a3 = document.getElementById("q5a3");
 var q5a4 = document.getElementById("q5a4");
+var q5a5 = document.getElementById("q5a5");
+var q5a6 = document.getElementById("q5a6");
 
 //questions 6
 var q6a1 = document.getElementById("q6a1");
 var q6a2 = document.getElementById("q6a2");
 var q6a3 = document.getElementById("q6a3");
 var q6a4 = document.getElementById("q6a4");
+var q6a5 = document.getElementById("q6a5");
+var q6a6 = document.getElementById("q6a6");
 
 //questions 7
 var q7a1 = document.getElementById("q7a1");
 var q7a2 = document.getElementById("q7a2");
 var q7a3 = document.getElementById("q7a3");
 var q7a4 = document.getElementById("q7a4");
+var q7a5 = document.getElementById("q7a5");
+var q7a6 = document.getElementById("q7a6");
 
 var results = document.getElementById("result");
 
@@ -62,32 +76,56 @@ q1a1.addEventListener("click", emika);
 q1a2.addEventListener("click", blue);
 q1a3.addEventListener("click", delilah);
 q1a4.addEventListener("click", amani);
-q1a4.addEventListener("click", audrey);
-q1a4.addEventListener("click", ever);
+q1a5.addEventListener("click", audrey);
+q1a6.addEventListener("click", ever);
 
 //questions 2
-q2a1.addEventListener("click", american);
-q2a2.addEventListener("click", skyward);
-q2a3.addEventListener("click", beautiful);
-q2a4.addEventListener("click", fountain);
+q2a1.addEventListener("click", delilah);
+q2a2.addEventListener("click", amani);
+q2a3.addEventListener("click", blue);
+q2a4.addEventListener("click", ever);
+q2a5.addEventListener("click", emika);
+q2a6.addEventListener("click", audrey);
 
 //questions 3
-q3a1.addEventListener("click", beautiful);
-q3a2.addEventListener("click", fountain);
-q3a3.addEventListener("click", american);
-q3a4.addEventListener("click", skyward);
+q3a1.addEventListener("click", amani);
+q3a2.addEventListener("click", emika);
+q3a3.addEventListener("click", delilah);
+q3a4.addEventListener("click", audrey);
+q3a5.addEventListener("click", ever);
+q3a6.addEventListener("click", blue);
 
 //questions 4
-q4a1.addEventListener("click", skyward);
-q4a2.addEventListener("click", beautiful);
-q4a3.addEventListener("click", fountain);
-q4a4.addEventListener("click", american);
+q4a1.addEventListener("click", audrey);
+q4a2.addEventListener("click", amani);
+q4a3.addEventListener("click", delilah);
+q4a4.addEventListener("click", ever);
+q4a5.addEventListener("click", blue);
+q4a6.addEventListener("click", emika);
 
 //questions 5
-q5a1.addEventListener("click", fountain);
-q5a2.addEventListener("click", american);
-q5a3.addEventListener("click", skyward);
-q5a4.addEventListener("click", beautiful);
+q5a1.addEventListener("click", delilah);
+q5a2.addEventListener("click", emika);
+q5a3.addEventListener("click", blue);
+q5a4.addEventListener("click", audrey);
+q5a5.addEventListener("click", ever);
+q5a6.addEventListener("click", amani);
+
+//questions 6
+q6a1.addEventListener("click", blue);
+q6a2.addEventListener("click", amani);
+q6a3.addEventListener("click", ever);
+q6a4.addEventListener("click", delilah);
+q6a5.addEventListener("click", emika);
+q6a6.addEventListener("click", audrey);
+
+//questions 6
+q7a1.addEventListener("click", ever);
+q7a2.addEventListener("click", delilah);
+q7a3.addEventListener("click", emika);
+q7a4.addEventListener("click", audrey);
+q7a5.addEventListener("click", blue);
+q7a6.addEventListener("click", amani);
 
 restart.addEventListener("click", restartQuiz);
 
@@ -162,6 +200,12 @@ function updateResults() {
   } else if (amaniScore >= 2) {
     results.innerHTML =
       "Recommendation: The Beautiful by Renée Ahdieh!<br><span class='rec'>Celine Rouseeau has found escape from her past in New Orleans, a city ruled by the dead. Twirling into the mysterious, glamorous city, Celine gets entailed in the underworld with the enigmatic Sebastien Saint Germain. Suddenly, one of her friends is murdered but as Celine is solving the mystery, she discovered an age-old feud that could disrupt all of New Orleans. </span>";
+  } else if (audreyScore >= 2) {
+    results.innerHTML =
+      "Recommendation: The Beautiful by Renée Ahdieh!<br><span class='rec'>Celine Rouseeau has found escape from her past in New Orleans, a city ruled by the dead. Twirling into the mysterious, glamorous city, Celine gets entailed in the underworld with the enigmatic Sebastien Saint Germain. Suddenly, one of her friends is murdered but as Celine is solving the mystery, she discovered an age-old feud that could disrupt all of New Orleans. </span>";
+  } else if (everScore >= 2) {
+    results.innerHTML =
+      "Recommendation: The Beautiful by Renée Ahdieh!<br><span class='rec'>Celine Rouseeau has found escape from her past in New Orleans, a city ruled by the dead. Twirling into the mysterious, glamorous city, Celine gets entailed in the underworld with the enigmatic Sebastien Saint Germain. Suddenly, one of her friends is murdered but as Celine is solving the mystery, she discovered an age-old feud that could disrupt all of New Orleans. </span>";
   } else {
     results.innerHTML = "Try again";
   }
@@ -170,10 +214,12 @@ function updateResults() {
 //restart
 function restartQuiz() {
   questionCount = 0;
-  skywardScore = 0;
-  fountainScore = 0;
-  americanScore = 0;
-  beautifulScore = 0;
+  emikaScore = 0;
+  blueScore = 0;
+  delilahScore = 0;
+  amaniScore = 0;
+  audreyScore = 0;
+  everScore = 0;
   results.innerHTML = "Your result is...";
   document.documentElement.scrollTop = 0;
 }
